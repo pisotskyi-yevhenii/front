3a1fdbca91a835c85080dfca936cb5c3303d5df8
yevhenii.pisotskyi <pisotskiy.jeny@gmail.com>
2026-01-19 15:24:10 +0100

    2-Blog preview card: implemented 2nd task (without hover interactive elements)

---
+++ 2-Blog-preview-card/src/js/scripts.js
@@ -0,0 +1,20 @@
+/**
+ *  1) Styles at the top of file.
+ *  All scss files must be included in /assets/src/scss/main.scss
+ */
+
+import '../scss/main.scss';
+
+/**
+ * 2) Import External (3d-part) JS/jQuery Libs, Extensions like: bootstrap, datepicker, fadeSlider, ect.
+ * !!! Import Only required components of Bootstrap (NOT whole library).
+ */
+// import Modal from 'bootstrap/js/dist/modal'; // uncomment for use
+
+/**
+ *  3) Import custom modules
+ */
+
+/**
+ * 4) Call functions
+ */
